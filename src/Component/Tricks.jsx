bbe14a8471ca1ac272dc437971c84c003480e88f
Hans Wang <yahanw@utas.edu.au>
2024-06-03 19:49:17 +1000

    new branch added

--- src/Component/Tricks.jsx
+++ src/Component/Tricks.jsx
@@ -2,25 +2,25 @@ import React, { useState } from 'react';
 import TrickItem from './TrickItem';
 
 const TrickList = () => {
-  const [tricksToLearn, setTricksToLearn] = useState(['Sit', 'Stay', 'Down', 'Hand', 'Hi-5', 'Jump', 'Place', '吐']);
-  const [learnedTricks, setLearnedTricks] = useState([]);
+  const [tricksToPractice, setTricksToPractice] = useState(['Sit', 'Stay', 'Down', 'Hand', 'Hi-5', 'Jump', 'Place', '吐', 'Go (绕柱)', '口哨(绕腿)','靠！','Middle']);
+  const [practicedTricks, setPracticedTricks] = useState([]);
 
   const handleLearnTrick = (trick) => {
-    setTricksToLearn(tricksToLearn.filter(t => t !== trick));
-    setLearnedTricks([...learnedTricks, trick]);
+    setTricksToPractice(tricksToPractice.filter(t => t !== trick));
+    setPracticedTricks([...practicedTricks, trick]);
   };
 
   return (
     <div>
-      <h2>Tricks to Practice</h2>
+      <h2>Tricks Known</h2>
       <ul className='Up'>
-        {tricksToLearn.map(trick => (
+        {tricksToPractice.map(trick => (
           <TrickItem key={trick} trick={trick} onLearnTrick={() => handleLearnTrick(trick)} />
         ))}
       </ul>
-      <h2>Practiced Tricks</h2>
+      <h2>Tricks Practiced</h2>
       <ul className='Down'>
-        {learnedTricks.map(trick => (
+        {practicedTricks.map(trick => (
           <li key={trick}>{trick}</li>
         ))}
       </ul>
